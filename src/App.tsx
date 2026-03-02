0b8365bb9a22240f99ccd2a867b17bdbf96a7083
OkauanDev <teko4278@gmail.com>
2026-03-02 18:13:33 -0300

    criando o banner

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import Header from './components/Header/Header'
+import BannerPromo from './components/BannerPromo/BannerPromo'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <BannerPromo/>
+      </div>
+    </>
+  )
+}
+
+export default App
